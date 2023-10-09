a634eb834758fc3ae3dbd240891a2e0e6b78f37f
cristian <cristiandevar@gmail.com>
2023-10-08 22:29:51 -0300

    agregamos contenido al home y al usermenu

--- resources/views/panel/index.blade.php
+++ resources/views/panel/index.blade.php
@@ -5,6 +5,42 @@
 @stop
 @section('content')
     <p>HOLA MUNDO DE ADMIN LTE</p>
+    <div class="content border p-2 m-2">
+        <h3>Titulo 1</h3>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. In fuga eum minima ab pariatur illum! Minus nesciunt beatae asperiores ipsa inventore iusto corporis, et, accusantium reprehenderit, quas eum? Molestias, facere!
+        </p>
+    </div>
+    <div class="content border p-2 m-2">
+        <h3>Titulo 2</h3>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. In fuga eum minima ab pariatur illum! Minus nesciunt beatae asperiores ipsa inventore iusto corporis, et, accusantium reprehenderit, quas eum? Molestias, facere!
+        </p>
+    </div>
+    <div class="content border p-2 m-2">
+        <h3>Titulo 3</h3>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. In fuga eum minima ab pariatur illum! Minus nesciunt beatae asperiores ipsa inventore iusto corporis, et, accusantium reprehenderit, quas eum? Molestias, facere!
+        </p>
+    </div>
+    <div class="content border p-2 m-2">
+        <h3>Titulo 4</h3>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. In fuga eum minima ab pariatur illum! Minus nesciunt beatae asperiores ipsa inventore iusto corporis, et, accusantium reprehenderit, quas eum? Molestias, facere!
+        </p>
+    </div>
+    <div class="content border p-2 m-2">
+        <h3>Titulo 5</h3>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. In fuga eum minima ab pariatur illum! Minus nesciunt beatae asperiores ipsa inventore iusto corporis, et, accusantium reprehenderit, quas eum? Molestias, facere!
+        </p>
+    </div>
+    <div class="content border p-2 m-2">
+        <h3>Titulo 6</h3>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. In fuga eum minima ab pariatur illum! Minus nesciunt beatae asperiores ipsa inventore iusto corporis, et, accusantium reprehenderit, quas eum? Molestias, facere!
+        </p>
+    </div>
 @stop
 @section('css')
     <link rel="stylesheet" href="/css/admin_custom.css">
